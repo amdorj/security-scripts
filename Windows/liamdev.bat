2532b57db69a2e33610f9ad3c360bd6bc164c9f5
amdorj <jamdoriv@gmail.com>
2018-04-24 10:13:32 -0500

    MBSA
    Added commands to automatically download and install Microsoft Baseline Security Analyzer.

--- Windows/liamdev.bat
+++ Windows/liamdev.bat
@@ -26,10 +26,21 @@ echo Setting up account password policies...
 net accounts /FORCELOGOFF:30 /MINPWLEN:8 /MAXPWAGE:90 /MINPWAGE:10 /UNIQUEPW:5
 echo Password policies set!  Moving on...
 
+::Check system Arch.
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+
 :: Disable Guest Acct
 net user Guest | findstr Active | findstr Yes
 if %errorlevel%==0 echo Guest account is active, deactivating
 if %errorlevel%==1 echo Guest account is not active, so not deactivating
 net user Guest /active:NO
+
+:: Download MBSA
+if %OS%==64BIT powershell -Command "Invoke-WebRequest https://download.microsoft.com/download/8/E/1/8E16A4C7-DD28-4368-A83A-282C82FC212A/MBSASetup-x64-EN.msi -OutFile MBSAx64-Setup-.msi"
+if %OS%==32BIT powershell -Command "Invoke-WebRequest https://download.microsoft.com/download/8/E/1/8E16A4C7-DD28-4368-A83A-282C82FC212A/MBSASetup-x86-EN.msi -OutFile MBSAx86-Setup.msi"
+
+if %OS%==32BIT MBSAx86-Setup.msi
+if %OS%==64BIT MBSAx64-Setup.msi
+
 
 :end
